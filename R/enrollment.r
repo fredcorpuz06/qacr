0c691d4b57a85e8460206b5d0940c03bb458b54b
Kenneth Stevenson <kstevenson@wesleyan.edu>
2019-11-20 15:21:52 -0500

    Add enrollment dataset and documentation

---
+++ R/enrollment.r
@@ -0,0 +1,20 @@
+#' @title enrollment
+#' @description Undergraduate and Graduate School Enrollment by state
+#' @format A data frame with 52 rows and 9 variables:
+#' \describe{
+#'   \item{\code{state or jurisdiction}}{integer COLUMN_DESCRIPTION}
+#'   \item{\code{2006–07}}{double Enrollment for 2006-2007 academic year}
+#'   \item{\code{2007–08}}{double Enrollment for 2007-2008 academic year}
+#'   \item{\code{2008–09}}{double Enrollment for 2008-2009 academic year}
+#'   \item{\code{2009–10}}{double Enrollment for 2009-2010 academic year}
+#'   \item{\code{2010–11}}{double Enrollment for 2010-2011 academic year}
+#'   \item{\code{2011–12}}{double Enrollment for 2011-2012 academic year}
+#'   \item{\code{2012–13}}{double Enrollment for 2012-2013 academic year}
+#'   \item{\code{2013–14}}{double Enrollment for 2013-2014 academic year} 
+#'}
+#' @docType data
+#' @keywords datasets
+#' @name enrollment
+#' @usage enrollment
+#' @source [link]: (https://nces.ed.gov/ipeds/Search/ViewTable?tableId=12607&returnUrl=%2Fipeds%2FSearch%20https://nces.ed.gov/ipeds/Search/DownloadTableLibraryTable?tableId=12607)
+NULL
